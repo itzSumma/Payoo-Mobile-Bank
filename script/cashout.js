a35b4e952df38973003c6a7cd40e88002b0a8206
itzSumma <itzSumma11@gmail.com>
2026-02-19 20:58:12 +0600

    add cashout

--- script/cashout.js
+++ script/cashout.js
@@ -1,21 +1,33 @@
-  
 
-document.getElementById("cashout-btn").addEventListener("click",function(){
-    //get agent number
-   const cashoutNumberInput = document.getElementById("cashout-number");
-   const cashoutNumber = cashoutNumberInput.value;
-   console.log(cashoutNumber);
-
-    //get amount , validate , convert number
-
-    const cashoutAmountInput = document.getElementById("cashout-amount");
-    const  cashoutAmount = cashoutAmountInput.value;
-    console.log(cashoutAmount);
-    //get current balance , validate , convert number
+ document.getElementById("cashout-btn").addEventListener("click", function(){
+        //get agent Number
+        const cashoutNumber = getValueFromInput("cashout-number");
+        if(cashoutNumber.length !== 11)
+        {alert("Invalid Number")
+            return;
+        }
+        //get amount
+        const cashoutAmount = getValueFromInput("cashout-amount");
+        
+        //get current balance
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance);
     
-    //calculate new balance
-    //get the pin verify
-    //if true show alert > set new balance
-    //if false show alert >return
+    //calculate balance
+    const newBalance = Number(balance) - Number(cashoutAmount);
+console.log(newBalance);
 
-});
+if(newBalance < 0){
+    alert("Invalid Amount");
+return;
+}
+const pin =getValueFromInput("cashout-pin");
+if (pin  === "2525"){
+    alert("Cashout Successfull");
+    balance.innerText = newBalance
+} else{
+    alert("Invalid Pin");
+    return;
+}
+ });
---
+++ script/machine.js
@@ -0,0 +1,45 @@
+console.log("machine connected")
+
+// machine id => Input value
+function getValueFromInput (id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(id , value);
+    return value ;
+
+}
+
+// //machine==> balance
+// function getBalance (){
+//     const balanceElement = document.getElementById("balance");
+//     const balance = balanceElement.innerText;
+// console.log("Current Balance", Number(balance));
+//     return Number(balance);
+// }
+// // machine value => set balance
+// function setBalance (value){
+//       const balanceElement = document.getElementById("balance");
+//       balanceElement.innerText = value;
+// }
+
+// // machine id => Input value
+// function getValueFromInput (id){
+//     const input = document.getElementById(id);
+//     const value = input.value;
+//     console.log(id , value);
+//     return value ;
+
+// }
+
+// //machine==> balance
+// function getBalance (){
+//     const balanceElement = document.getElementById("balance");
+//     const balance = balanceElement.innerText;
+// console.log("Current Balance", Number(balance));
+//     return Number(balance);
+// }
+// // machine value => set balance
+// function setBalance (value){
+//       const balanceElement = document.getElementById("balance");
+//       balanceElement.innerText = value;
+// }
